5bc9805b5fa228aafad2e8f339b27bef276fc3bc
sylvian <sylvian31@gmail.com>
2018-11-22 10:58:25 -0500

    first commit

---
+++ src/main/java/com/expertus/expertusprojet/ExpertusprojetApplication.java
@@ -0,0 +1,12 @@
+package com.expertus.expertusprojet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpertusprojetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExpertusprojetApplication.class, args);
+	}
+}
