63e58cbd414f24b7d97c9e6fce7019b0490a42dd
sylvian <sylvian31@gmail.com>
2018-11-26 18:51:18 -0500

    eureka order

--- microServiceOrder/src/main/java/com/expertus/microServiceOrder/MicroServiceOrderApplication.java
+++ microServiceOrder/src/main/java/com/expertus/microServiceOrder/MicroServiceOrderApplication.java
@@ -2,13 +2,14 @@ package com.expertus.microServiceOrder;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @ComponentScan("com.expertus.microServiceOrder")
 @EnableJpaRepositories("com.expertus.microServiceOrder.repository")
-//@EnableEurekaClient
+@EnableEurekaClient
 public class MicroServiceOrderApplication {
 
 	public static void main(String[] args) {
